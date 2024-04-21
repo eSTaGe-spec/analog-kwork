e41e4656a6c10544dce7fcfc44c5f801d88382da
Joseph Norotov <boss.norotov@list.ru>
2024-04-21 16:51:54 +0300

    Создание моделей Executor и Customer

--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Executor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Логин пользователя')
+    name = models.CharField(max_length=100, verbose_name='Имя пользователя')
+    about_me = models.TextField(verbose_name='Информация о пользователе')
+    phone = models.CharField(max_length=15, verbose_name='Номер телефона')
+    email = models.EmailField(verbose_name='Email')
+    status = models.CharField(max_length=20, default='executor', verbose_name='Статус пользователя')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'executor'
+        verbose_name = 'Исполнитель'
+        verbose_name_plural = 'Исполнители'
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Логин пользователя')
+    name = models.CharField(max_length=100, verbose_name='Имя пользователя')
+    about_me = models.TextField(verbose_name='Информация о пользователе')
+    phone = models.CharField(max_length=15, verbose_name='Номер телефона')
+    email = models.EmailField(verbose_name='Email')
+    status = models.CharField(max_length=20, default='customer', verbose_name='Статус пользователя')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'customer'
+        verbose_name = 'Заказчик'
+        verbose_name_plural = 'Заказчики'
+
